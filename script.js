1b31c8afeed93a8e44f464dc8ed7d2e1b6f3d9b5
dudesw575 <dylanneves@gmail.com>
2021-03-18 23:01:32 -0500

    Ball collides with sides and top of canvas. Ball collides with top of paddle Game Over alert if ball lands outside paddle width

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-const paddleHeight = 10;
-const paddleWidth = canvas.width / 9;
+//canvas width = 750
+//canvas height = 600
 
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var interval = setInterval(draw, 10);
+let paddleHeight = 10;
+let paddleWidth = canvas.width / 9;
 let paddleX = (canvas.width - paddleWidth) / 2;
-let paddleY = (canvas.height - paddleHeight) /10;
+let paddleY = (canvas.height - paddleHeight) / 10;
 
-window.addEventListener("keydown", moveSomething, false);
-  
-function moveSomething(e) {
-  switch(e.keyCode) {
-      case 37: //left
-        if (paddleX > 8 ) {
-          paddleX -= 10;
-        }
-        else {}
-          break;
-      case 39: //Right
-      if (paddleX + paddleWidth + 8 < canvas.width ) {
-        paddleX += 10;
-      }
-      else {}
-          break;
-  }
-  e.preventDefault();
-  drawPaddle();
+function drawBall() {
+    ctx.beginPath(); //circle center = 375, 570 starting
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
+
+window.addEventListener("keydown", movePaddle, false);
+
+function movePaddle(e) {
+    switch (e.keyCode) {
+        case 37: //left
+            if (paddleX > 8) {
+                paddleX -= 10;
+            } else {
+            }
+            break;
+        case 39: //Right
+            if (paddleX + paddleWidth + 8 < canvas.width) {
+                paddleX += 10;
+            } else {
+            }
+            break;
+    }
+    e.preventDefault();
+    drawPaddle();
 }
 
 function drawPaddle() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     // the rectangle
-    ctx.rect(paddleX, canvas.height - paddleHeight - 8, paddleWidth, paddleHeight);
+    ctx.rect(
+        paddleX,
+        canvas.height - paddleHeight - 8,
+        paddleWidth,
+        paddleHeight
+    );
     ctx.fillStyle = "gray";
     ctx.fill();
 }
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius - paddleHeight - 8) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+
+    x += dx;
+    y += dy;
+}
 
 //draw paddle on load
-window.onload = drawPaddle();
+//window.onload = draw();
